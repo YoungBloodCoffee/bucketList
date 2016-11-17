620c7e02a606deb273302bd5c204a2148fe6307b
Nate Kosin <nate.kosin@yahoo.com>
2016-11-16 17:24:40 -0800

    initial commit

---
+++ TaskModel.swift
@@ -0,0 +1,9 @@
+//
+//  TaskModel.swift
+//  bucketList
+//
+//  Created by Nate Kosin on 11/16/16.
+//  Copyright © 2016 Nate Kosin. All rights reserved.
+//
+
+import Foundation
---
+++ bucketList/CancelButton.swift
@@ -0,0 +1,9 @@
+//
+//  CancelButton.swift
+//  bucketList
+//
+//  Created by Nate Kosin on 11/7/16.
+//  Copyright © 2016 Nate Kosin. All rights reserved.
+//
+
+import Foundation
