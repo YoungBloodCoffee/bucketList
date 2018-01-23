4aa0b17e84ac016a16d66097e781465412b2a78a
YoungBloodCoffee <nate.kosin@yahoo.com>
2018-01-23 10:25:59 -0800

    Update Edit Cells for MissionDetailsViewController
    Allow edits to be made to selected cells.

--- bucketList/MissionDetailsViewController.swift
+++ bucketList/MissionDetailsViewController.swift
@@ -47,13 +47,12 @@ class MissionDetailsViewController: UITableViewController {
     }
     */
 
-    /*
+   
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
     /*
     // Override to support editing the table view.
